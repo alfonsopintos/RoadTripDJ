9b4a392b547f6ca086e7bb8203bb45240fae2369
alfonsopintos <alfonsopintos@gmail.com>
2015-07-21 18:30:10 -0400

    finished part 4 - custom uibarbutton items - toolbar not linked

---
+++ RoadTrip DJ/PlayerBar.h
@@ -0,0 +1,16 @@
+//
+//  PlayerBar.h
+//  RoadTrip DJ
+//
+//  Created by Alfonso Pintos on 7/21/15.
+//  Copyright © 2015 Meme Menu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+@interface PlayerBar : UIToolbar
+
+@property (nonatomic, assign) IBInspectable CGFloat spacing;
+
+@end
